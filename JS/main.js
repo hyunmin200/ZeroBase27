4c8dde32a470598b6a741a6a77de3a3d52daad0b
hyunmin200 <gyejeongjin@gmail.com>
2024-06-04 16:39:40 +0900

    FEAT: Function

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,21 @@
-const user = {
-	name: "현민",
-	age: 18,
-	isValid: true,
-	email: "gyejeongjin@gmail.com",
-	hello: function () {
-		return `내 이름은 ${this.name}이다. 내 나이는 ${this.age}`;
-	},
+// 함수 선언문(Declaration)
+function add(a, b) {
+	// console.log(a);
+	// console.log(b);
+	return a + b;
+}
+
+console.log(add);
+console.log(add(1, 2));
+console.log(add(31, 14));
+console.log(add(5, 8));
+
+// 함수 표현식
+const sub = function (a, b) {
+	return a - b;
 };
 
-// 점 표기법(Dot Notation)을 사용해, 객체의 속성이나 메소드에 접근할 수 있다.
-console.log(user.name);
-console.log(user.age);
-console.log(user.isValid);
-console.log(user.email);
-console.log(user.hello());
-
-// 대괄호 표기법(Bracket Notation)을 사용해, 객체의 속성이나 메소드에 접근할 수 있다.
-console.log(user["name"]);
-console.log(user["age"]);
-console.log(user["isValid"]);
-console.log(user["email"]);
-console.log(user["hello"]());
+console.log(sub);
+console.log(sub(2, 1));
+console.log(sub(14, 10));
+console.log(sub(8, 5));
