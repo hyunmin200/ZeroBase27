40d66428df9f73a310d9f8649ad15d21509c3257
hyunmin200 <gyejeongjin@gmail.com>
2024-06-04 20:09:33 +0900

    FEAT :: 비교 & 부정 연산자

--- JS/main.js
+++ JS/main.js
@@ -1,61 +1,49 @@
-// 산술 연산자 (Arithmetic operator)
+// 부정 연산자(Negation Operator)는 참과 거짓의 반대값을 불린 데이터로 반환한다.
 
-console.log(1 + 2);
-console.log(5 - 7);
-console.log(3 * 4);
-console.log(10 / 2);
-console.log(7 % 5);
+console.log(!true); // false
+console.log(!false); // true
 
-// 할당 연산자 (Assignment Operators)
+console.log("중첩 사용!!");
+console.log(!0); // true
+console.log(!!0); // false
+console.log(!!!0); // true
+console.log(!!!!0); // false
 
-let a = 3;
-console.log(a);
+console.log("거짓(Falsy)!!");
+console.log(!null); // true
+console.log(!NaN); // true
+console.log(!undefined); // true
+console.log(!""); // true
 
-// a = a + 2
-a += 2;
-console.log(a);
+console.log("참(Truthy");
+console.log(!{}); // false
+console.log(![]); // false
+console.log(!"A"); // false
 
-// a = a - 2
-a -= 2;
-console.log(a);
+// 비교 연산자(Comparison Operators)는 두 데이터를 비교할 때 사용한다.
+const a = 1;
+const b = 3;
 
-// a = a * 2
-a *= 2;
-console.log(a);
+// 동등(형 변환!)
+console.log(a == b);
 
-// a = a / 2
-a /= 2;
-console.log(a);
+// 부등(형 변환!)
+console.log(a != b);
 
-// a = a % 2
-a %= 2;
-console.log(a);
+// 일치
+console.log(a === b);
 
-// 증감 연산자 (Increment & Decrement Operators)는 변수를 1씩 더하거나 빼는 연산자이다.
+// 불일치
+console.log(a !== b);
 
-// ++ 기호가 뒤에 있는 경우
-let b = 3;
-console.log(b++);
-console.log(b);
+// 큼
+console.log(a > b);
 
-// ++ 기호가 앞에 있는 경우
-let c = 3;
-console.log(++c);
-console.log(c);
+// 크거나 같음
+console.log(a >= b);
 
-// -- 기호가 뒤에 있는 경우
-let d = 3;
-console.log(d--);
-console.log(d);
+// 작음
+console.log(a < b);
 
-// -- 기호가 앞에 있는 경우
-let f = 3;
-console.log(--f);
-console.log(f);
-
-// 증감 연산자 보다는, 할당 연산자를 사용하는 것을 추천한다.
-let e = 3;
-e += 1;
-console.log(e);
-e -= 1;
-console.log(e);
+// 작거나 같음
+console.log(a <= b);
