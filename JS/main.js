bacc5d00c6451aa30ebf09939d40cd48a39b28be
hyunmin200 <gyejeongjin@gmail.com>
2024-06-04 19:56:59 +0900

    FEAT :: 산술 & 할당 & 증감 연산자

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,61 @@
-const data = {
-	string: "123",
-	number: 123,
-	boolean: true,
-	null: null,
-	undefined: undefined,
-	array: [1, 2, 3],
-	object: { a: 1, b: 2, c: 3 },
-	function: function () {},
-};
+// 산술 연산자 (Arithmetic operator)
 
-// typeof 사용하기
-console.log("typeof 데이터");
-console.log(typeof data.string === "string");
-console.log(typeof data.number === "number");
-console.log(typeof data.boolean === "boolean");
-console.log(typeof data.null === "object");
-console.log(typeof data.undefined === "undefined");
-console.log(typeof data.array === "object");
-console.log(typeof data.object === "object");
-console.log(typeof data.function === "function");
-// null, array, object 빼고는 잘 확인이 된다. (간단히 사용할 때, 유용함)
+console.log(1 + 2);
+console.log(5 - 7);
+console.log(3 * 4);
+console.log(10 / 2);
+console.log(7 % 5);
 
-// constructor 사용하기
-console.log("데이터.constructor");
-console.log(data.string.constructor === String);
-console.log(data.number.constructor === Number);
-console.log(data.boolean.constructor === Boolean);
-// console.log(data.null.constructor); // Error
-// console.log(data.undefined.constructor); // Error
-console.log(data.array.constructor === "object");
-console.log(data.object.constructor === "object");
-console.log(data.function.constructor === "function");
+// 할당 연산자 (Assignment Operators)
+
+let a = 3;
+console.log(a);
+
+// a = a + 2
+a += 2;
+console.log(a);
+
+// a = a - 2
+a -= 2;
+console.log(a);
+
+// a = a * 2
+a *= 2;
+console.log(a);
+
+// a = a / 2
+a /= 2;
+console.log(a);
+
+// a = a % 2
+a %= 2;
+console.log(a);
+
+// 증감 연산자 (Increment & Decrement Operators)는 변수를 1씩 더하거나 빼는 연산자이다.
+
+// ++ 기호가 뒤에 있는 경우
+let b = 3;
+console.log(b++);
+console.log(b);
+
+// ++ 기호가 앞에 있는 경우
+let c = 3;
+console.log(++c);
+console.log(c);
+
+// -- 기호가 뒤에 있는 경우
+let d = 3;
+console.log(d--);
+console.log(d);
+
+// -- 기호가 앞에 있는 경우
+let f = 3;
+console.log(--f);
+console.log(f);
+
+// 증감 연산자 보다는, 할당 연산자를 사용하는 것을 추천한다.
+let e = 3;
+e += 1;
+console.log(e);
+e -= 1;
+console.log(e);
