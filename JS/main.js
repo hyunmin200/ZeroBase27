d2a47f37d7d3144f024775f9c64dc3643f1901a6
hyunmin200 <gyejeongjin@gmail.com>
2024-06-04 17:18:53 +0900

    FEAT :: Variable

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,14 @@
-// 함수 선언문(Declaration)
-function add(a, b) {
-	// console.log(a);
-	// console.log(b);
-	return a + b;
-}
+// const 키워드는 상수(Constant)를 의미하며, 한 번 선언하면 다른 값으로 변경 할 수 없다.
+const c = 12;
+console.log(c);
+console.log(c);
+// c = 34; // Error: Assignment to constant variable.
+// JS는 Error가 발생하면 아래 코드를 싹다 무시한다.
 
-console.log(add);
-console.log(add(1, 2));
-console.log(add(31, 14));
-console.log(add(5, 8));
-
-// 함수 표현식
-const sub = function (a, b) {
-	return a - b;
-};
-
-console.log(sub);
-console.log(sub(2, 1));
-console.log(sub(14, 10));
-console.log(sub(8, 5));
+// let 키워드는 선언한 값을 다른 값으로 바꿀 수 있다.
+let l = 12;
+console.log(l);
+console.log(l);
+l = 13;
+console.log(l);
+console.log(l);
