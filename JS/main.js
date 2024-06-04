86c31f4884c2acfb03aae366e8f77cd7a6f6f702
hyunmin200 <gyejeongjin@gmail.com>
2024-06-04 19:38:04 +0900

    FEAT :: DataType

--- JS/main.js
+++ JS/main.js
@@ -1,71 +1,33 @@
-// '참'으로 평가되는 값 (Truthy)
-if (true) {
-	console.log("참!");
-}
-if ({}) {
-	console.log("참!");
-}
-if ([]) {
-	console.log("참!");
-}
-if (42) {
-	console.log("참!");
-}
-if ("0") {
-	console.log("참!");
-}
-if ("false") {
-	console.log("참!");
-}
-if (new Date()) {
-	console.log("참!");
-}
-if (-42) {
-	console.log("참!");
-}
-if (12n) {
-	console.log("참!");
-}
-if (3.14) {
-	console.log("참!");
-}
-if (-3.14) {
-	console.log("참!");
-}
-if (Infinity) {
-	console.log("참!");
-}
-if (-Infinity) {
-	console.log("참!");
-}
-// ...
+const data = {
+	string: "123",
+	number: 123,
+	boolean: true,
+	null: null,
+	undefined: undefined,
+	array: [1, 2, 3],
+	object: { a: 1, b: 2, c: 3 },
+	function: function () {},
+};
 
-// '거짓'으로 평가되는 값 (Falsy)
-if (false) {
-	console.log("거짓..");
-}
-if (null) {
-	console.log("거짓..");
-}
-if (undefined) {
-	console.log("거짓..");
-}
-if (42) {
-	console.log("거짓..");
-}
-if (0) {
-	console.log("거짓..");
-}
-if (-0) {
-	console.log("거짓..");
-}
-if (NaN) {
-	console.log("거짓..");
-}
-if (0n) {
-	console.log("거짓..");
-}
-if ("") {
-	console.log("거짓..");
-}
-// ...
+// typeof 사용하기
+console.log("typeof 데이터");
+console.log(typeof data.string === "string");
+console.log(typeof data.number === "number");
+console.log(typeof data.boolean === "boolean");
+console.log(typeof data.null === "object");
+console.log(typeof data.undefined === "undefined");
+console.log(typeof data.array === "object");
+console.log(typeof data.object === "object");
+console.log(typeof data.function === "function");
+// null, array, object 빼고는 잘 확인이 된다. (간단히 사용할 때, 유용함)
+
+// constructor 사용하기
+console.log("데이터.constructor");
+console.log(data.string.constructor === String);
+console.log(data.number.constructor === Number);
+console.log(data.boolean.constructor === Boolean);
+// console.log(data.null.constructor); // Error
+// console.log(data.undefined.constructor); // Error
+console.log(data.array.constructor === "object");
+console.log(data.object.constructor === "object");
+console.log(data.function.constructor === "function");
