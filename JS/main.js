11091abcb4ca4ec56cfb7b296726b4fb38f4daba
hyunmin200 <gyejeongjin@gmail.com>
2024-06-10 15:42:48 +0900

    FEAT :: 기명 함수 vs 익명 함수

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,15 @@
-function hello() {
-	return "Hello";
+const h1El = document.querySelector("h1");
+
+// 기명 함수 - function 이름() {}
+function handler() {
+	console.log(h1El.textContent);
 }
+h1El.addEventListener("click", handler);
 
-// 함수 데이터(Data)
-console.log(hello); // 함수 덩어리(코드) 그 자체가 출력된다
-console.log(typeof hello); // function 타입이 나오게 된다.
-// 이렇게 사용하면 함수안에 코드를 사용하는게 아니라 함수 안 코드를 확인만 하는 것이 된다.
+// 익명 함수 - function () {}
+h1El.addEventListener("click", function () {
+	console.log(h1El.textContent);
+});
 
-// 함수 호출(Call)
-console.log(hello()); // 반환 데이터인 Hello가 출력된다.
-console.log(typeof hello()); // 반환하는 데이터의 타입인 string이 출력된다.
+// 이름있으면 기명 함수, 없으면 익명 함수
+// 상황에 맞게 사용하면 된다.
