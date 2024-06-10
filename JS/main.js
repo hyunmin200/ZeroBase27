bccf37fd7db4a96135561a8896c8643636898b47
hyunmin200 <gyejeongjin@gmail.com>
2024-06-10 15:27:23 +0900

    FEAT :: While

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,7 @@
-// for in 문
-// for (const 키변수 in 객체) {}
-const user = {
-	name: "이백원",
-	age: 85,
-	isValid: true,
-	email: "gyejeongjin@gmail.com",
-};
-for (const key in user) {
-	if (key === "age") continue;
-	console.log(key, user[key]);
+// while 문
+// while (조건) {}
+let count = 1;
+while (count <= 10) {
+	count += 1;
+	console.log(count);
 }
