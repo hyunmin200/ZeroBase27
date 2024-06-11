d7144a7c7e025d9d9962489f88ccb8d5b783274a
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 19:12:24 +0900

    FEAT :: .map

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,24 @@
-const f = ["사과", "바나나", "체리"];
-console.log(f.join());
-console.log(f.join(""));
-console.log(f.join(", "));
-console.log(f.join("/"));
+const numbers = [17, 20, 100, 5, 200];
+const doubleNumbers = numbers.map((number) => number * 2);
+console.log(doubleNumbers);
+console.log(numbers);
 
-const msg = "Hello World";
-console.log(msg.split("").reverse().join(""));
+const fruits = ["apple", "banana", "cherry"];
+const capitalizedFruits = fruits.map((fruit) => fruit.toUpperCase());
+console.log(capitalizedFruits);
+console.log(fruits);
+
+const users = [
+	{ name: "현민", age: 18 },
+	{ name: "200원", age: 200, email: "200@gmail.com" },
+	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
+];
+
+const userEmails = users.map((user) => user.email);
+console.log(userEmails);
+console.log(userEmails.filter((email) => email));
+
+const f = ["사과", "바나나", "체리"];
+const UpperF = f.map((e, i) => {
+	console.log(i);
+});
