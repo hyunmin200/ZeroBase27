4a018d26e2dbe9b7fdcdafeba84da7898b72283c
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 17:49:53 +0900

    FEAT :: findIndex

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,11 @@
 const numbers = ["12", "456", "132", "60", "1"];
-const foundNumbers = numbers.find((number) => number < 30);
-console.log(foundNumbers);
+const foundIndex = numbers.findIndex((number) => number < 30);
+console.log(foundIndex);
 
 const users = [
 	{ name: "현민", age: 18 },
 	{ name: "200원", age: 200, email: "200@gmail.com" },
 	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
 ];
-const foundUser = users.find((user) => !user.email);
-console.log(foundUser);
+const foundUserIndex = users.findIndex((user) => !user.email);
+console.log(foundUserIndex);
