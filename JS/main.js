a6efce1c72da1bd51ee8027a934836d43af1eef1
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 20:13:05 +0900

    FEAT :: 배열 메소드 인덱스

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,13 @@
-const f1 = ["사과", "바나나", "체리"];
-console.log(f1.unshift("두리안"));
-console.log(f1.length);
-console.log(f1);
-
 const numbers = [17, 20, 199, 60, 31];
-console.log(numbers.unshift(9, 10, 11));
-console.log(numbers.length);
-console.log(numbers);
+numbers.every((num, idx) => {
+	console.log(num, idx);
+	return true;
+});
+numbers.filter((num, idx) => {
+	console.log(num, idx);
+	return true;
+});
+numbers.reduce((acc, cur, idx) => {
+	console.log(acc, cur, idx);
+	return acc + cur;
+}, 0);
