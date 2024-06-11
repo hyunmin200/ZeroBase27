ec427f029e45eae0272a924e13d4c8ab95871da6
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 19:35:51 +0900

    FEAT :: .reverse

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,8 @@
-const numbers = [10, 19, 29, 13, 120, 123];
+const fruits1 = ["APPLE", "BANANA", "CHERRY"];
+console.log(fruits1.reverse());
+console.log(fruits1);
 
-let sum1 = 0;
-numbers.forEach((number) => {
-	sum += number;
-});
-console.log(sum);
-
-// number가 배열안에 값, accumulator가 누적값
-const sum2 = numbers.reduce((accumulator, number) => {
-	return accumulator + number;
-}, 0); // 콜백 함수, 초기값
-console.log(sum2);
-
-const users = [
-	{ name: "현민", age: 18 },
-	{ name: "200원", age: 200, email: "200@gmail.com" },
-	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
-];
-const sum3 = users.reduce((acc, user) => acc + user.age, 0);
-console.log(sum3);
+// 원본이 변경 안되었으면 좋겠다?
+const fruits2 = ["APPLE", "BANANA", "CHERRY"];
+console.log([...fruits2].reverse());
+console.log(fruits2);
