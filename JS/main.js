8df2c46f81dbb878771a408435a9ee1bd40d5791
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 16:53:28 +0900

    FEAT :: .toISOString

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,1 @@
-const date1 = new Date();
-const date2 = Date.now();
-
-console.log(date1);
-console.log(date1.getTime());
-console.log(date2);
-
-setTimeout(() => {
-	console.log(Date.now() - date2);
-}, 1000);
+console.log(new Date().toISOString);
