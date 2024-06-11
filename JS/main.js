033bd2f58d397354cffd2ed7080cb7c5d6d2f9ea
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 19:41:58 +0900

    FEAT :: .some

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,11 @@
-const numbers = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+const numbers = [17, 20, 199, 1, 40];
+const isValid = numbers.some((number) => number > 200);
+console.log(isValid);
 
-console.log(numbers.slice(0, 3));
-console.log(numbers.slice(4, -1));
-console.log(numbers.slice(4));
-console.log(numbers.slice(-4));
-console.log(numbers.slice(-4, -1));
-console.log(numbers);
+const users = [
+	{ name: "현민", age: 18 },
+	{ name: "200원", age: 200, email: "200@gmail.com" },
+	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
+];
+console.log(users.some((user) => user.email));
+console.log(users.some((user) => user.phone));
