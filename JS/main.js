c8827ccc9a34872b44d41602255c11b65602ddb4
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 10:21:45 +0900

    FEAT :: 함수-반환과 종료

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,24 @@
-// 호이스팅(Hoisting)
-hello(); // OK!
-world(); // Error..
+function sayHi(name) {
+	return `Hi, ${name}`; // return시 함수는 종료된다.
+	console.log("동작하지 않음");
+}
+const h = sayHi("200woni"); // 반환
+console.log(h); // "Hi, 200woni"
 
-// 함수 선언문(Declaration)
-function hello() {
-	console.log("Hello~");
+console.log(sayHi("200woni")); // "Hi, 200woni"
+//===============================================//
+function a() {
+	// return // undefined
+}
+function b() {
+	return; // undefined
+}
+function c() {
+	return undefined;
 }
 
-// 함수 표현식(Expression)
-const world = function () {
-	console.log("World");
-};
+console.log(a()); // undefined
+console.log(b()); // undefined
+console.log(c()); // undefined
+
+// 결국 3가지가 다 똑같은 명시하느냐 안하느냐의 차이
