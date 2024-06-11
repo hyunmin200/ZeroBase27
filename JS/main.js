bad23c1cec59b4de250d463b176a7ae6d4d21dc5
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 20:20:27 +0900

    FEAT :: Array.isArray

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,11 @@
-const numbers = [17, 20, 199, 60, 31];
-numbers.every((num, idx) => {
-	console.log(num, idx);
-	return true;
-});
-numbers.filter((num, idx) => {
-	console.log(num, idx);
-	return true;
-});
-numbers.reduce((acc, cur, idx) => {
-	console.log(acc, cur, idx);
-	return acc + cur;
-}, 0);
+const f = ["사과", "바나나", "체리"];
+// 유사배열
+const arrayLikeFruits = {
+	0: "사과",
+	1: "바나나",
+	2: "체리",
+	length: 3,
+};
+
+console.log(Array.isArray(f));
+console.log(Array.isArray(arrayLikeFruits));
