0a6f8202dca0b93d3dae43178070780d507cf2a5
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 10:10:31 +0900

    FEAT :: 함수-선언과 표현 & 호이스팅

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,13 @@
-const h1El = document.querySelector("h1");
+// 호이스팅(Hoisting)
+hello(); // OK!
+world(); // Error..
 
-// 기명 함수 - function 이름() {}
-function handler() {
-	console.log(h1El.textContent);
+// 함수 선언문(Declaration)
+function hello() {
+	console.log("Hello~");
 }
-h1El.addEventListener("click", handler);
 
-// 익명 함수 - function () {}
-h1El.addEventListener("click", function () {
-	console.log(h1El.textContent);
-});
-
-// 이름있으면 기명 함수, 없으면 익명 함수
-// 상황에 맞게 사용하면 된다.
+// 함수 표현식(Expression)
+const world = function () {
+	console.log("World");
+};
