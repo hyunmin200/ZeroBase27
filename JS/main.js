9a22853866c85ec889b9b92537b31fd60165568b
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 17:14:41 +0900

    FEAT :: .every()

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,11 @@
-const name = ["현민", "황현민"];
-const age = ["18", "19"];
-const na = this.name.concat(age);
-console.log(na);
+const numbers = ["12", "456", "132", "60", "1"];
+const isValid = numbers.every((item) => item < 200);
+console.log(isValid);
+
+const users = [
+	{ name: "현민", age: 18 },
+	{ name: "200원", age: 200, email: "200@gmail.com" },
+	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
+];
+console.log(users.every((user) => user.email));
+console.log(users.every((user) => user.age));
