eaedf07ae4c1e676667f61170da9cb3937f18e25
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 20:05:45 +0900

    FEAT :: .unshift

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,9 @@
-// splice(인덱스, 삭제개수, 추가요손)
-
-// 요소 추가
 const f1 = ["사과", "바나나", "체리"];
-f1.splice(2, 0, "두리안");
+console.log(f1.unshift("두리안"));
+console.log(f1.length);
 console.log(f1);
 
-// 요소 삭제
-const f2 = ["사과", "바나나", "체리"];
-f2.splice(1, 1);
-console.log(f2);
-
-// 요소 교체
-const f3 = ["사과", "바나나", "체리"];
-f3.splice(1, 1, "두리안", "오렌지", "망고");
+const numbers = [17, 20, 199, 60, 31];
+console.log(numbers.unshift(9, 10, 11));
+console.log(numbers.length);
+console.log(numbers);
