7b3568b2182631d052afddf3c1a6c5423a581a6c
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 17:41:25 +0900

    FEAT :: .filter

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,15 @@
 const numbers = ["12", "456", "132", "60", "1"];
-const isValid = numbers.every((item) => item < 200);
-console.log(isValid);
+const filterNumbers = numbers.filter((number) => number < 30);
+console.log(filterNumbers);
 
 const users = [
 	{ name: "현민", age: 18 },
 	{ name: "200원", age: 200, email: "200@gmail.com" },
 	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
 ];
-console.log(users.every((user) => user.email));
-console.log(users.every((user) => user.age));
+const youngUsers = users.filter((user) => user.age < 30);
+console.log(youngUsers);
+const userWithEmail = users.filter((user) => user.email);
+console.log(userWithEmail);
+const userWithPhone = users.filter((user) => user.phone);
+console.log(userWithPhone);
