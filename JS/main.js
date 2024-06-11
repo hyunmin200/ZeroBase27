dcf7e22ffc4b962efb3b6dba96f73da9841eaf74
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 17:43:44 +0900

    FEAT :: .find

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,11 @@
 const numbers = ["12", "456", "132", "60", "1"];
-const filterNumbers = numbers.filter((number) => number < 30);
-console.log(filterNumbers);
+const foundNumbers = numbers.find((number) => number < 30);
+console.log(foundNumbers);
 
 const users = [
 	{ name: "현민", age: 18 },
 	{ name: "200원", age: 200, email: "200@gmail.com" },
 	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
 ];
-const youngUsers = users.filter((user) => user.age < 30);
-console.log(youngUsers);
-const userWithEmail = users.filter((user) => user.email);
-console.log(userWithEmail);
-const userWithPhone = users.filter((user) => user.phone);
-console.log(userWithPhone);
+const foundUser = users.find((user) => !user.email);
+console.log(foundUser);
