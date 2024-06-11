d12da34410535d92ef337c70f9f26af0e1424590
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 18:53:37 +0900

    FEAT :: .join

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,8 @@
 const f = ["사과", "바나나", "체리"];
-console.log(f.includes("사과"));
-console.log(f.includes("버나나")); // false
+console.log(f.join());
+console.log(f.join(""));
+console.log(f.join(", "));
+console.log(f.join("/"));
 
-const n = [10, 20, 30, 40];
-console.log(n.includes(10));
-console.log(n.includes(123123));
+const msg = "Hello World";
+console.log(msg.split("").reverse().join(""));
