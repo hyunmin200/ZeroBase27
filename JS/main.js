0be816ee0b37bbc401c29d3986258471b963e657
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 10:53:16 +0900

    FEAT :: 인수와 매개변수

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,29 @@
-function sayHi(name) {
-	return `Hi, ${name}`; // return시 함수는 종료된다.
-	console.log("동작하지 않음");
-}
-const h = sayHi("200woni"); // 반환
-console.log(h); // "Hi, 200woni"
+// 인수(Argument)와 매개변수(Parameter)
 
-console.log(sayHi("200woni")); // "Hi, 200woni"
-//===============================================//
-function a() {
-	// return // undefined
-}
-function b() {
-	return; // undefined
-}
-function c() {
-	return undefined;
-}
+// function add(a, b) {
+// 	return a + b;
+// }
+// console.log(add(2, 1));
+// console.log(add(7, 4));
+// console.log(add("A", "B"));
 
-console.log(a()); // undefined
-console.log(b()); // undefined
-console.log(c()); // undefined
+// =================================== //
 
-// 결국 3가지가 다 똑같은 명시하느냐 안하느냐의 차이
+// 매개변수의 기본값(Default Value)
+// function add(a, b = 1) {
+// 	return a + b;
+// }
+// console.log(add(2));
+// console.log(add(7, undefined)); // 위랑 같음
+// console.log(add());
+
+// =================================== //
+
+// 나머지 매개변수(Rest Parameter)
+
+function add(a, b, ...rest) {
+	console.log(a, b, rest);
+	return rest.reduce((acc, cur) => acc + cur, 0);
+}
+const res = add(1, 2, 3, 4, 5, 6, 7, 8);
+console.log(res);
