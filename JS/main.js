3ab0be1c8f17d2d8670aa4ee5a647ec11980375a
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 19:27:04 +0900

    FEAT :: .reduce

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,21 @@
-const fruits = ["apple", "banana", "cherry"];
-console.log(fruits.push("durian"));
-console.log(fruits.length);
-console.log(fruits);
+const numbers = [10, 19, 29, 13, 120, 123];
 
-const numbers = [17, 20, 100, 5, 200];
-console.log(numbers.push(9, 10, 11));
-console.log(numbers.length);
-console.log(numbers);
+let sum1 = 0;
+numbers.forEach((number) => {
+	sum += number;
+});
+console.log(sum);
+
+// number가 배열안에 값, accumulator가 누적값
+const sum2 = numbers.reduce((accumulator, number) => {
+	return accumulator + number;
+}, 0); // 콜백 함수, 초기값
+console.log(sum2);
+
+const users = [
+	{ name: "현민", age: 18 },
+	{ name: "200원", age: 200, email: "200@gmail.com" },
+	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
+];
+const sum3 = users.reduce((acc, user) => acc + user.age, 0);
+console.log(sum3);
