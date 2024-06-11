dfa925d01970634c8a6e9aa2c4e917cf1b04aaed
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 20:03:00 +0900

    FEAT :: .splice

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,15 @@
-const numbers = [17, 20, 199, 1, 40];
-const isValid = numbers.some((number) => number > 200);
-console.log(isValid);
+// splice(인덱스, 삭제개수, 추가요손)
 
-const users = [
-	{ name: "현민", age: 18 },
-	{ name: "200원", age: 200, email: "200@gmail.com" },
-	{ name: "황현민", age: 19, email: "gyejeongjin@gmail.com" },
-];
-console.log(users.some((user) => user.email));
-console.log(users.some((user) => user.phone));
+// 요소 추가
+const f1 = ["사과", "바나나", "체리"];
+f1.splice(2, 0, "두리안");
+console.log(f1);
+
+// 요소 삭제
+const f2 = ["사과", "바나나", "체리"];
+f2.splice(1, 1);
+console.log(f2);
+
+// 요소 교체
+const f3 = ["사과", "바나나", "체리"];
+f3.splice(1, 1, "두리안", "오렌지", "망고");
