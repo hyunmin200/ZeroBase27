4f21e1979339163393acb062ca6d0adec80c2828
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 15:53:20 +0900

    FEAT :: Math.random

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
-console.log(Math.min(10, 128, 12, 49, 1));
+console.log(Math.random());
+
+// 원하는 범위의 랜덤값 뽑기
+function Random(min = 0, max = 10) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(Random()); // 0~9
+console.log(Random(0, 100)); // 0~99
+console.log(Random(101, 200)); // 101~199
