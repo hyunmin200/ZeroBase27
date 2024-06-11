023f47200970bf57eb0aee483fbafaf4e8c7ec4c
hyunmin200 <gyejeongjin@gmail.com>
2024-06-11 20:28:08 +0900

    FEAT :: Array.from

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,22 @@
-const f = ["사과", "바나나", "체리"];
+const fruits = ["Apple", "Banana", "Cherry"];
 // 유사배열
 const arrayLikeFruits = {
-	0: "사과",
-	1: "바나나",
-	2: "체리",
+	0: "Apple",
+	1: "Banana",
+	2: "Cherry",
 	length: 3,
 };
+console.log(fruits);
+console.log(arrayLikeFruits);
 
-console.log(Array.isArray(f));
+console.log(fruits[1]);
+console.log(arrayLikeFruits[1]);
+
+console.log(fruits.length);
+console.log(arrayLikeFruits.length);
+
+console.log(Array.isArray(fruits));
 console.log(Array.isArray(arrayLikeFruits));
+
+console.log(fruits.map((fruit) => fruit.toUpperCase()));
+console.log(fruits.from(arrayLikeFruits).map((fruit) => fruit.toUpperCase()));
