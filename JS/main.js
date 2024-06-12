2df0f034ff3d862b28987cbc59de6829a3c4a83b
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 15:03:56 +0900

    FEAT :: createElement

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,15 @@
-const selEl = document.querySelector("div");
-console.log(selEl.children);
-console.log(selEl.children[selEl.children.length - 1]);
-console.log(selEl.firstChild);
-console.log(selEl.lastChild);
+const divEl = document.createElement("div");
+divEl.textContent = "안녕하세요!";
+divEl.classList.add("hello");
+console.log(divEl);
+
+const inputEl = document.createElement("input");
+inputEl.value = "입력하세요!";
+console.log(inputEl);
+
+const buttonEl = document.createElement("button");
+buttonEl.textContent = "버튼입니다!";
+console.log(buttonEl);
+
+// HTML body에 추가하기
+document.body.append(divEl, inputEl, buttonEl);
