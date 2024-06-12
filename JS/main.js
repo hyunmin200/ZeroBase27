874ca654e05a4c1d46ddff13e7e5eff2c14f2e43
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 14:58:13 +0900

    FEAT :: 자식

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
-const selEl = document.querySelector("#apple");
-console.log(selEl.previousElementSibling);
-console.log(selEl.nextElementSibling);
+const selEl = document.querySelector("div");
+console.log(selEl.children);
+console.log(selEl.children[selEl.children.length - 1]);
+console.log(selEl.firstChild);
+console.log(selEl.lastChild);
