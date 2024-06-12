d216e94ac721992eecdd7d739c8d4e6eea4e5cff
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 11:25:45 +0900

    FEAT :: Object.keys

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,14 @@
-const target = { a: 1, b: 2 };
-const source1 = { b: 3, c: 4 };
-const source2 = { c: 5, d: 6 };
-// 원본 객체 변함
-// const result = Object.assign(target, source1, source2);
-// console.log(target);
-// console.log(result);
-
-// 원본 객체 변하지 않음
-const result = Object.assign({}, target, source1, source2);
-console.log(target);
-console.log(result);
-
-const userA = {
+const user = {
 	name: "200원",
 	age: 200,
-};
-const userB = {
-	age: 22,
-	email: "gyejeongjin@gmail.com",
+	email: "200woni@naver.com",
 	isValid: true,
 };
-// const thw = Object.assign(userA, userB);
-const thw = Object.assign({}, userA, userB);
+const keys = Object.keys(user);
+console.log(keys);
 
-console.log(thw);
-console.log(userA);
+keys.forEach((key) => {
+	const el = document.createElement("div");
+	el.innerHTML = `<strong>${key}</strong>: ${user[key]}`;
+	document.body.append(el);
+});
