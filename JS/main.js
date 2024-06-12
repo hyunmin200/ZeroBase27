d5f586f230bd3cc5132756f0e39562b1f93d8719
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 16:43:14 +0900

    FEAT :: remove

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,4 @@
-const parentEl = document.createElement(".parent");
+const el = document.querySelector("#apple");
 
-const divEl = document.createElement("div");
-divEl.textContent = "새로운 요소!";
-
-const inputEl = document.createElement("input");
-
-const res1 = parentEl.prepend(new Comment(" 새로운 주석 "));
-const res2 = parentEl.append(divEl, "새로운 텍스트");
-const res3 = (parentEl.appendChild(inputEl).placeholder = "값을 입력하세요!");
-
-console.log(res1, res2);
-console.log(res3);
+console.log(el);
+el.remove();
