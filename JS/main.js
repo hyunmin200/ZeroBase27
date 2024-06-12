3bab9a5467625f1cbe59959906dab355e47c94c2
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 19:37:29 +0900

    FEAT :: 이벤트 기본 동작 방지

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,11 @@
-const parentEl = document.querySelector(".parent");
-
-parentEl.addEventListener("click", (event) => {
-	console.log(parentEl);
-	console.log(event.target);
+// <a> 태그에서 페이지 이동 방지!
+const anchorEl = document.querySelector("a");
+anchorEl.addEventListener("click", (e) => {
+	e.preventDefault();
 });
 
-const inputEl = document.querySelector("input");
-inputEl.addEventListener("keydown", (event) => {
-	console.log(event.key);
-	console.log(inputEl.value);
-	console.log(event.target.value);
+// 마우스의 휠의 스크롤 동작 방지!
+const parentEl = document.querySelector(".parent");
+parentEl.addEventListener("wheel", (e) => {
+	e.preventDefault();
 });
