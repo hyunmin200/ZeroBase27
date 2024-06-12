ee4b3f1e2138e69542e43dc03b0e1fce66d8fd4f
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 20:21:53 +0900

    FEAT :: 한글 입력 이벤트

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,9 @@
-const parentEl = document.querySelector(".parent");
-const childEl = document.querySelector(".child");
-const anchorEl = document.querySelector("a");
-
-window.addEventListener("click", () => {
-	console.log("윈도우");
-});
-document.documentElement.addEventListener("click", () => {
-	console.log("HTML");
-});
-document.body.addEventListener(
-	"click",
-	() => {
-		console.log("body");
-	},
-	{ capture: true },
-);
-parentEl.addEventListener("click", (e) => {
-	console.log("parent");
-	// e.stopPropagation(); // 버블링 정지!
-});
-childEl.addEventListener("click", () => {
-	console.log("child");
-});
-anchorEl.addEventListener("click", (e) => {
-	e.preventDefault();
-	console.log("a");
+const inputEl = document.querySelector("input");
+inputEl.addEventListener("keydown", (e) => {
+	// if (e.isComposing) return;
+	if (e.key === "Enter") {
+		const h1El = document.createElement("h1");
+		h1El.textContent = inputEl.value;
+		document.body.append(h1El);
+	}
 });
