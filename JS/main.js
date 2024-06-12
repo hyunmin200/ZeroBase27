c02149344c75bab964f355aad99f267de1dd020a
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 19:30:34 +0900

    FEAT :: 이벤트 객체

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,13 @@
 const parentEl = document.querySelector(".parent");
-const childEl = document.querySelector(".child");
 
-// 삭제하기 위해서는 기명함수가 필요하다.
-const handler = () => {
-	console.log("부모");
-};
+parentEl.addEventListener("click", (event) => {
+	console.log(parentEl);
+	console.log(event.target);
+});
 
-parentEl.addEventListener("click", handler);
-childEl.addEventListener("click", () => {
-	parentEl.removeEventListener("click", handler);
+const inputEl = document.querySelector("input");
+inputEl.addEventListener("keydown", (event) => {
+	console.log(event.key);
+	console.log(inputEl.value);
+	console.log(event.target.value);
 });
