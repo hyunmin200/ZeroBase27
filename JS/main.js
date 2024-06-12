2f6aa414df3bd45f355493076c518edc7e97babe
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 17:25:25 +0900

    FEAT :: style

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,21 @@
 const el = document.querySelector("#apple");
 
-el.classList.add("active");
-console.log(el.classList.contains("active"));
+// 개별 지정 할 수 있다.
+el.style.width = "100px";
+el.style.fontSize = "20px";
+el.style.backgroundColor = "green";
+el.style.position = "absolute";
 
-el.classList.remove("active");
-console.log(el.classList.contains("active"));
-
-el.addEventListener("click", () => {
-	el.classList.toggle("active");
-	console.log(el.classList.contains("active"));
+// 한 번에 지정할 수 있습니다.
+Object.assign(el.style, {
+	width: "100px",
+	fontSize: "20px",
+	backgroundColor: "green",
+	position: "absolute",
 });
+
+console.log(el.style);
+console.log(el.style.width);
+console.log(el.style.fontSize);
+console.log(el.style.backgroundColor);
+console.log(el.style.position);
