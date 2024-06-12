c2aaa816e692b05fcd58f5e39513e69e8bd9bcba
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 16:47:45 +0900

    FEAT :: contains

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,9 @@
-const el = document.querySelector("#apple");
+const parentEl = document.querySelector(".parent");
+const appleEl = document.querySelector("#apple");
 
-console.log(el);
-el.remove();
+console.log(parentEl.contains(appleEl));
+console.log(document.body.contains(parentEl));
+console.log(document.body.contains(appleEl));
+console.log(document.body.contains(document.body));
+console.log(parentEl.contains(document.body));
+console.log(appleEl.contains(document.body));
