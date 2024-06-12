5be092eae6f6dcf7cf2e6f29e3dd7afb22955f3f
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 20:37:11 +0900

    FEAT :: 키보드 이벤트

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,16 @@
-const parentEl = document.querySelector(".parent");
-const childEls = document.querySelectorAll(".child");
+const inputEl = document.querySelector("input");
 
-childEls.forEach((childEl) => {
-	// click - 클릭했을 때,
-	// dblclick - 더블 클릭했을 때
-	childEl.addEventListener("click", () => {
-		childEl.classList.toggle("active");
-	});
-
-	// contextmenu - 우클릭 했을 때
-	childEl.addEventListener("contextmenu", (e) => {
-		e.preventDefault();
-		console.log(childEl.textContent);
-	});
-});
-
-// mousedown - 버튼을 누를 때
-// mouseup - 버튼을 땔 때
-// mouseenter - 포인터가 요소로 들어갈 때
-// mouseleave - 포인터가 요소에서 나올 때
-parentEl.addEventListener("mousedown", () => {
-	parentEl.classList.add("active");
-});
-parentEl.addEventListener("mouseup", () => {
-	parentEl.classList.remove("active");
-});
-
-// mousemove - 포인터가 움직일 때
-parentEl.addEventListener("mousemove", (e) => {
-	// console.log(e.x, e.y);
-});
-
-parentEl.addEventListener("wheel", (e) => {
-	// console.log("Parent Wheel");
+// keydown - 키를 누를 때
+// keyup - 키를 땔 때
+inputEl.addEventListener("keydown", (e) => {
+	console.log(e.key);
+	if (e.key === "Enter") {
+		console.log("엔터");
+	}
+	if (e.key === "Escape") {
+		console.log("ESC");
+	}
+	if (e.key === " ") {
+		console.log("스페이스");
+	}
 });
