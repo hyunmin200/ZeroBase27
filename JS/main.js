289e8934d9656986134cc51340ebd72c4abb5259
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 20:30:34 +0900

    FEAT :: 마우스와 포인터 이벤트

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,36 @@
-const inputEl = document.querySelector("input");
-inputEl.addEventListener("keydown", (e) => {
-	// if (e.isComposing) return;
-	if (e.key === "Enter") {
-		const h1El = document.createElement("h1");
-		h1El.textContent = inputEl.value;
-		document.body.append(h1El);
-	}
+const parentEl = document.querySelector(".parent");
+const childEls = document.querySelectorAll(".child");
+
+childEls.forEach((childEl) => {
+	// click - 클릭했을 때,
+	// dblclick - 더블 클릭했을 때
+	childEl.addEventListener("click", () => {
+		childEl.classList.toggle("active");
+	});
+
+	// contextmenu - 우클릭 했을 때
+	childEl.addEventListener("contextmenu", (e) => {
+		e.preventDefault();
+		console.log(childEl.textContent);
+	});
+});
+
+// mousedown - 버튼을 누를 때
+// mouseup - 버튼을 땔 때
+// mouseenter - 포인터가 요소로 들어갈 때
+// mouseleave - 포인터가 요소에서 나올 때
+parentEl.addEventListener("mousedown", () => {
+	parentEl.classList.add("active");
+});
+parentEl.addEventListener("mouseup", () => {
+	parentEl.classList.remove("active");
+});
+
+// mousemove - 포인터가 움직일 때
+parentEl.addEventListener("mousemove", (e) => {
+	// console.log(e.x, e.y);
+});
+
+parentEl.addEventListener("wheel", (e) => {
+	// console.log("Parent Wheel");
 });
