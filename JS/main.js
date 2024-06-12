8a5f7811b7f337752af1b6b17ed89c045862662c
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 17:34:47 +0900

    FEAT :: Attribute

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,15 @@
 const el = document.querySelector("#apple");
 
-// 개별 지정 할 수 있다.
-el.style.width = "100px";
-el.style.fontSize = "20px";
-el.style.backgroundColor = "green";
-el.style.position = "absolute";
+console.log(el.getAttribute("class"));
+console.log(el.getAttribute("title"));
 
-// 한 번에 지정할 수 있습니다.
-Object.assign(el.style, {
-	width: "100px",
-	fontSize: "20px",
-	backgroundColor: "green",
-	position: "absolute",
-});
+// 덮어쓰여진다.
+// el.setAttribute("class", "hello-world")
+el.setAttribute("title", "Hello-World");
 
-console.log(el.style);
-console.log(el.style.width);
-console.log(el.style.fontSize);
-console.log(el.style.backgroundColor);
-console.log(el.style.position);
+console.log(el.hasAttribute("class"));
+console.log(el.hasAttribute("title"));
+console.log(el.hasAttribute("value"));
+
+el.removeAttribute("class");
+el.removeAttribute("title");
