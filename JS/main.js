e7a93eccca6299ea3f6df41bf713481392078271
hyunmin200 <gyejeongjin@gmail.com>
2024-06-12 17:18:04 +0900

    FEAT :: classList

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,12 @@
 const el = document.querySelector("#apple");
-const str = "Hello World";
-const num = 123;
-const obj = {
-	a: 1,
-	b: 2,
-};
 
-console.log(el.dataset);
+el.classList.add("active");
+console.log(el.classList.contains("active"));
 
-el.dataset.helloWorld = str;
-el.dataset.number = num;
-// 객체 같은 경우는 JSON문자 형식으로 변환해야된다.
-el.dataset.object = JSON.stringify(obj);
+el.classList.remove("active");
+console.log(el.classList.contains("active"));
 
-console.log(el.dataset);
-
-console.log(el.dataset.helloWorld);
-console.log(el.dataset.number);
-console.log(JSON.parse(el.dataset.number));
-console.log(el.dataset.object);
-console.log(JSON.parse(el.dataset.object));
+el.addEventListener("click", () => {
+	el.classList.toggle("active");
+	console.log(el.classList.contains("active"));
+});
