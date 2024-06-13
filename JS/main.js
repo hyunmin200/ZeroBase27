fd6e891b91aa04b38a8d50ade6532f831e4a1ed3
hyunmin200 <gyejeongjin@gmail.com>
2024-06-13 17:13:00 +0900

    FEAT :: 클래스 상속

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,34 @@
-class User {
-	constructor(first, last) {
-		// this 키워드는 클래스로 생성된 인스턴스라고 생각하면 된다.
-		this.firstName = first;
-		this.lastName = last;
-	}
-	static isUser(user) {
-		return user instanceof User;
+class A {
+	constructor(a) {
+		this.a = a;
 	}
 }
 
-const thw = new User("200", "원");
-const hhm = new user("황", "현민");
-const hhh = {
-	name: "황황현",
-	age: 100,
-};
+class B extends A {
+	constructor(a, b) {
+		// super를 사용하게 되면 부모 클래스의 생성자에 접근해서 값을 넣어준다.
+		super(a);
+		this.b = b;
+	}
+}
 
-console.log(User.isUser(thw));
-console.log(User.isUser(hhm));
-console.log(User.isUser(hhh));
+const a = new A(1);
+const b = new B(1, 2);
+
+// a의 prototype이 Object라는 것은 a도 부모 클래스로 object를 상속받고 있다는 것이다.
+console.log(a);
+// b의 prototype은 A클래스로 부모 클래스로 A클래스를 상속받고 있다는 것이다.
+console.log(b);
+
+// A클래스에서 만들었기에 A클래스의 인스턴스가 맞음
+console.log(a instanceof A); // true
+// B클래스에서 만들었지만 A클래스의 확장 버전이기 때문에 A클래스의 인스턴스라고 볼 수 있음
+console.log(b instanceof A); // true
+
+// A클래스는 B라는 클래스의 상위 클래스이기 때문에 B라는 클래스의 인스턴라고 볼 수 없다.
+console.log(a instanceof B); // false
+// B클래스에서 만들었기에 Bs클래스의 인스턴스가 맞음
+console.log(b instanceof B); // true
+
+console.log(a instanceof Object); // true
+console.log(b instanceof Object); // true
