8149389a1df7dd52c86176d827318155e0954b7d
hyunmin200 <gyejeongjin@gmail.com>
2024-06-13 19:24:18 +0900

    FEAT :: 콜백 지옥 해결

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,26 @@
-function renderImage(callback) {
-	const imgEl = document.createElement("img");
-	imgEl.src = "https://picsum.photos/3000/2000";
-	imgEl.addEventListener("load", () => {
-		document.body.append(imgEl);
-		callback();
-	});
-}
-renderImage(() => {
-	console.log("Done 1");
-	renderImage(() => {
-		console.log("Done 2");
-		renderImage(() => {
-			console.log("Done 3");
-			renderImage(() => {
-				console.log("Done 4");
-			});
+function renderImage() {
+	return new Promise((resolve) => {
+		const imgEl = document.createElement("img");
+		imgEl.src = "https://picsum.photos/3000/2000";
+		imgEl.addEventListener("load", () => {
+			document.body.append(imgEl);
+			resolve();
 		});
 	});
-});
+}
+renderImage()
+	.then(() => {
+		console.log("Done 1");
+		return renderImage();
+	})
+	.then(() => {
+		console.log("Done 2");
+		return renderImage();
+	})
+	.then(() => {
+		console.log("Done 3");
+		return renderImage();
+	})
+	.then(() => {
+		console.log("Done 4");
+	});
