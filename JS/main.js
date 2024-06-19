1ec50ac2b57ba78846b3f7c81eff1ca360c57d34
hyunmin200 <gyejeongjin@gmail.com>
2024-06-19 20:24:35 +0900

    FEAT :: 불변성과 가변성

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,17 @@
-// Throttle
-// - 정해진 시간 간격으로 함수를 실행하도록 제한합니다.
-window.addEventListener(
-	"scroll",
-	_.throttle(function () {
-		console.log("Scroll!");
-	}, 400),
-);
+const a = 1;
+let b = a;
+b = 2;
+console.log(a);
+console.log(b);
 
-// Debounce
-// - 정해진 시간 동안 함수가 실행되지 않으면, 함수를 실행합니다.(마지막에 한 번만 실행)
-async function getMovies(movieName) {
-	const res = await fetch(
-		`https://omdbapi.com/?apikey=7035c60c&s=${movieName}`,
-	);
-	return await res.json;
-}
-const inputEl = document.querySelector("input");
-inputEl.addEventListener(
-	"input",
-	_.debounce(function () {
-		console.log(getMovies(inputEl.value));
-	}, 400),
-);
+const c = { x: 1, y: 2 };
+const d = c;
+d.x = 99;
+console.log(c);
+console.log(d);
+
+const e = [1, 2, 3];
+const f = e;
+f[0] = 99;
+console.log(e);
+console.log(f);
