7bf74ca3a213e96278c185bc6fd1d36f4428745f
hyunmin200 <gyejeongjin@gmail.com>
2024-06-19 19:08:52 +0900

    FEAT :: 함수 재귀

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,13 @@
-async function getMovies(movieName) {
-	const res = await fetch(
-		`https://omdbapi.com/?apikey=7035c60c&s=${movieName}`,
-	);
-	return await res.json();
-}
+const neo = { name: "Neo" };
+const evan = { name: "Evan", parent: neo };
+const lewis = { name: "Lewis", parent: evan };
+const amy = { name: "Amy", parent: lewis };
 
-const titles = ["frozen", "avengers", "avatar"];
-
-// 배열 메소드 반복
-// titles.forEach(async (title, index) => {
-// 	const movies = await getMovies(title);
-// 	console.log(`${index + 1} ${title}`, movies);
-// });
-
-// for 반복문
-(async () => {
-	let index = 1;
-	for (const title of titles) {
-		const movies = await getMovies(title);
-		console.log(`${index + 1} ${title}`, movies);
-		index += 1;
+const getRootUser = (user) => {
+	if (user.parent) {
+		return getRootUser(user.parent);
 	}
-})();
+	return user;
+};
+
+console.log(getRootUser(amy));
