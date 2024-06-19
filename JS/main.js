196aee1864a6a07e54c5834f8fe6815627a439ed
hyunmin200 <gyejeongjin@gmail.com>
2024-06-19 19:32:33 +0900

    FEAT :: call, bind, apply

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,25 @@
-function User() {
-	this.name = "User";
-	return {
-		name: "Neo",
-		age: 80,
-		// getInfo() {
-		// 	return `${this.name}는 ${this.age}입니다.`;
-		// },
-		getInfo: () => {
-			return `${this.name}는 ${this.age}입니다.`;
-		},
-	};
-}
-
-const u = new User();
-console.log(u.name);
-console.log(u.age);
-console.log(u.getInfo());
-
-const evan = {
-	name: "Evan",
-	age: 25,
+const neo = {
+	name: "Neo",
 };
-console.log(u.getInfo.call(evan));
+const amy = {
+	name: "Amy",
+	getInfo(age, city) {
+		return `${this.name}는 ${age}세이고, ${city}에 삽니다.`;
+	},
+};
+console.log(amy.getInfo(22, "서울"));
+
+// .call(this, 인수1, 인수2, ...)
+// 대상 함수를 주어진 객체(this)의 메소드로 실행합니다.
+console.log(amy.getInfo.call(neo, 1, "부산"));
+
+// .apply(this, [인수1, 인수2, ...])
+// 대상 함수를 주어진 객체(this)의 메소드로 실행합니다.
+console.log(amy.getInfo.apply(neo, [85, "서울"]));
+
+// .bind(this)
+// 대상 함수를 주어진 객체(this)의 메소드로 실행할 수 있는 새로운 함수를 반환합니다.
+const neoGetInfo = amy.getInfo.bind(neo);
+setTimeout(() => {
+	console.log(neoGetInfo(85, "서울"));
+}, 1000);
