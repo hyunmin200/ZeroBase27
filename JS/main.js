0c3e14ff24d7314b1cbaacdd1993c06beecc59f5
hyunmin200 <gyejeongjin@gmail.com>
2024-06-20 09:52:47 +0900

    FEAT :: 렉시컬 스코프

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,34 @@
-const a = 1;
-let b = a;
-b = 2;
-console.log(a);
-console.log(b);
-
-const c = { x: 1, y: 2 };
-const d = c;
-d.x = 99;
-console.log(c);
-console.log(d);
-
-const e = [1, 2, 3];
-const f = e;
-f[0] = 99;
-console.log(e);
-console.log(f);
+const a = {
+	fnA() {
+		console.log("fnA", this);
+		const b = {
+			fnB() {
+				console.log("fnB", this);
+				const c = {
+					fnC() {
+						console.log("fnC", this);
+						console.log("a", a);
+						console.log("b", b);
+						console.log("c", c);
+						console.log("x", x);
+					},
+				};
+				return c;
+			},
+		};
+		return b;
+	},
+	fnX() {
+		console.log("fnX", this);
+		const x = {
+			fnY() {
+				console.log("fnY", this);
+				console.log("a", a);
+				console.log("b", b);
+				console.log("x", x);
+			},
+		};
+	},
+};
+a.fnA().fnB().fnC();
+a.fnY();
