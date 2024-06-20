a1044beb36b91c185f9bf30ec1afb89c2606a002
hyunmin200 <gyejeongjin@gmail.com>
2024-06-20 10:19:20 +0900

    FEAT :: 클로저

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,11 @@
-const a = {
-	fnA() {
-		console.log("fnA", this);
-		const b = {
-			fnB() {
-				console.log("fnB", this);
-				const c = {
-					fnC() {
-						console.log("fnC", this);
-						console.log("a", a);
-						console.log("b", b);
-						console.log("c", c);
-						console.log("x", x);
-					},
-				};
-				return c;
-			},
-		};
-		return b;
-	},
-	fnX() {
-		console.log("fnX", this);
-		const x = {
-			fnY() {
-				console.log("fnY", this);
-				console.log("a", a);
-				console.log("b", b);
-				console.log("x", x);
-			},
-		};
-	},
+const h1El = document.querySelector("h1");
+
+const createToggleHandler = () => {
+	let isRed = false;
+	return (event) => {
+		isRed = !isRed;
+		event.target.style.color = isRed ? "red" : "black";
+	};
 };
-a.fnA().fnB().fnC();
-a.fnY();
+
+h1El.addEventListener("click", createToggleHandler());
